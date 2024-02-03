35df7578bee8775c87698af52f276974619841fe
Ycaro02 <ycaro@Kali.home>
2024-02-03 16:45:48 -0600

    best scipt ever

--- rsc/ls_diff.sh
+++ rsc/ls_diff.sh
@@ -4,3 +4,6 @@ export LC_COLLATE=en_US.utf8 && ls ${1} ${2} | tr -d "\'\" " > real_ls_out
 ./ft_ls ${1} ${2} | tr -d "\'\" " > myls_out
 diff myls_out real_ls_out
 echo $?
+
+# possible to grep -v ls_out to avoid size issue but not realy clean and don't remove total size line for -l
+# ./ft_ls ${1} ${2} | tr -d "\'\" " > myls_out
